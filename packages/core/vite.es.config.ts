582be66820c8b0eb726278c90fbd24ddb046bea3
ZukaiGong <zukai524@163.com>
2025-05-21 23:43:21 +0800

    feat: css分块，umd压缩，自定义vite插件打包前删除文件及打包后移动文件

--- packages/core/vite.es.config.ts
+++ packages/core/vite.es.config.ts
@@ -4,6 +4,10 @@ import { resolve } from "path";
 import dts from "vite-plugin-dts"; // 打包时生成ts的类型声明文件
 import { readdirSync, readdir } from "fs";
 import { delay, defer, filter, map } from "lodash-es";
+import shell from "shelljs";
+import hooks from "./hooksPlugin";
+
+const TRY_MOVE_STYLES_DELAY = 800 as const;
 
 function getDirectoriesSync(basePath: string) {
   const entries = readdirSync(basePath, { withFileTypes: true });
@@ -14,6 +18,13 @@ function getDirectoriesSync(basePath: string) {
   );
 }
 
+function moveStyles() {
+  readdir("./dist/es/theme", (err) => {
+    if (err) return delay(moveStyles, TRY_MOVE_STYLES_DELAY);
+    defer(() => shell.mv("./dist/es/theme", "./dist"));
+  });
+}
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -22,9 +33,15 @@ export default defineConfig({
       tsconfigPath: "../../tsconfig.build.json",
       outDir: "dist/types",
     }),
+    hooks({
+      rmFiles: ["./dist/es", "./dist/theme", "./dist/types"],
+      afterBuild: moveStyles,
+    }),
   ],
   build: {
     outDir: "dist/es",
+    minify: false,
+    cssCodeSplit: true,
     lib: {
       entry: resolve(__dirname, "./index.ts"),
       name: "ToyElement",
@@ -46,6 +63,12 @@ export default defineConfig({
         assetFileNames: (assetInfo) => {
           // 将style.css文件名改为index.css
           if (assetInfo.name === "style.css") return "index.css";
+          if (
+            assetInfo.type === "asset" &&
+            /\.(css)$/i.test(assetInfo.name as string)
+          ) {
+            return "theme/[name].[ext]";
+          }
           return assetInfo.name as string;
         },
         // 分包，将同一个一个文件夹下的文件打包进一个文件
