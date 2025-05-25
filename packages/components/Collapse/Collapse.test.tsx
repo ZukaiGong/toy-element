ed8be9c7faa991fa9f2b53f33ae13619e26cd00f
ZukaiGong <zukai524@163.com>
2025-05-25 22:04:38 +0800

    feat: 补全Collapse组件测试用例

--- packages/components/Collapse/Collapse.test.tsx
+++ packages/components/Collapse/Collapse.test.tsx
@@ -1,5 +1,7 @@
 import { DOMWrapper, mount, type VueWrapper } from "@vue/test-utils";
 import { beforeAll, describe, it, test, expect, vi } from "vitest";
+import transitionEvents from "./transitionEvents";
+
 import Collapse from "./Collapse.vue";
 import CollapseItem from "./CollapseItem.vue";
 
@@ -84,5 +86,126 @@ describe("Collapse.vue", () => {
     expect(onChange).toHaveBeenCalledTimes(2);
     expect(onChange).toHaveBeenCalledWith([]);
     expect(onChange).toHaveBeenLastCalledWith(["b"]);
+  });
+
+  test("disabled 内容", async () => {
+    // disabled
+    expect(disabledHeader.classes()).toContain("is-disabled");
+    onChange.mockClear();
+    await disabledHeader.trigger("click");
+    expect(disabledContent.isVisible()).toBeFalsy();
+    expect(onChange).not.toHaveBeenCalled();
+  });
+
+  test("modelValue 变更", async () => {
+    wrapper.setValue(["b"], "modelValue");
+    await wrapper.vm.$nextTick();
+    expect(secondHeader.classes()).toContain("is-active");
+    expect(firstHeader.classes()).not.toContain("is-active");
+  });
+
+  test("手风琴模式", async () => {
+    wrapper = mount(
+      () => (
+        <Collapse accordion modelValue={["a"]} {...{ onChange }}>
+          <CollapseItem name="a" title="title a">
+            content a
+          </CollapseItem>
+          <CollapseItem name="b" title="title b">
+            content b
+          </CollapseItem>
+        </Collapse>
+      ),
+      {
+        global: {
+          stubs: ["ErIcon"],
+        },
+        attachTo: document.body,
+      }
+    );
+
+    headers = wrapper.findAll(".er-collapse-item__header");
+    contents = wrapper.findAll(".er-collapse-item__wapper");
+
+    firstHeader = headers[0];
+    secondHeader = headers[1];
+
+    firstContent = contents[0];
+    secondContent = contents[1];
+    await firstHeader.trigger("click");
+    await secondHeader.trigger("click");
+    expect(onChange).toHaveBeenCalledTimes(2);
+    expect(onChange).toHaveBeenCalledWith(["b"]);
+    expect(firstHeader.classes()).not.toContain("is-active");
+    expect(secondHeader.classes()).toContain("is-active");
+  });
+
+  test("手风琴模式 错误处理", () => {
+    const warn = vi.spyOn(console, "warn").mockImplementation(() => {});
+    mount(
+      () => (
+        <Collapse accordion modelValue={["a", "b"]} {...{ onChange }}>
+          <CollapseItem name="a" title="title a">
+            content a
+          </CollapseItem>
+          <CollapseItem name="b" title="title b">
+            content b
+          </CollapseItem>
+          <CollapseItem name="c" title="title c" disabled>
+            content c
+          </CollapseItem>
+        </Collapse>
+      ),
+      {
+        global: {
+          stubs: ["ErIcon"],
+        },
+      }
+    );
+    expect(warn.mock.calls).toMatchInlineSnapshot(
+      `
+        [
+          [
+            [ErUIError: [ErCollapse]:accordion mode should only have one active item],
+          ],
+        ]
+      `
+    );
+  });
+});
+
+describe("Collapse/transitionEvents.ts", () => {
+  const wrapper = mount(() => <div></div>);
+  test("beforeEnter", () => {
+    transitionEvents.beforeEnter(wrapper.element);
+    expect(wrapper.element.style.height).toBe("0px");
+    expect(wrapper.element.style.overflow).toBe("hidden");
+  });
+  test("enter", () => {
+    transitionEvents.enter(wrapper.element);
+    expect(wrapper.element.style.height).toBe(
+      `${wrapper.element.scrollHeight}px`
+    );
+  });
+  test("afterEnter", () => {
+    transitionEvents.afterEnter(wrapper.element);
+    expect(wrapper.element.style.height).toBe("");
+    expect(wrapper.element.style.overflow).toBe("");
+  });
+  test("beforeLeave", () => {
+    transitionEvents.beforeLeave(wrapper.element);
+    expect(wrapper.element.style.height).toBe(
+      `${wrapper.element.scrollHeight}px`
+    );
+    expect(wrapper.element.style.overflow).toBe("hidden");
+  });
+  test("leave", () => {
+    transitionEvents.leave(wrapper.element);
+    expect(wrapper.element.style.height).toBe("0px");
+  });
+  test("afterLeave", () => {
+    transitionEvents.afterLeave(wrapper.element);
+    expect(wrapper.element.style.height).toBe("");
+    expect(wrapper.element.style.overflow).toBe("");
   });
 });
